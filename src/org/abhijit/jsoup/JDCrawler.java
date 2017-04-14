0df63f2f9d3dcc104fb306a9ead62db27e985d17
banerjab <banerjab@FCP-23377-D.india.rapidigm.com>
2017-04-14 13:26:29 +0530

    getLocalities method is working now.

--- src/org/abhijit/jsoup/JDCrawler.java
+++ src/org/abhijit/jsoup/JDCrawler.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,6 +24,7 @@ import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class JDCrawler {
@@ -31,14 +33,20 @@ public class JDCrawler {
 	private static String primaryUrl = "https://www.justdial.com/";
 	private static String baseUrl = "https://www.justdial.com/Pune/Stationery-Shops/nct-1764/page-";
 	private static Set<String> paginationUrlSetByCity = new LinkedHashSet<String>();
-	private static Map<String , String> mainUrlMapByCity = new TreeMap<String, String>();
+	private static Map<String, String> mainUrlMapByCity = new TreeMap<String, String>();
 	private static Set<StationeryShop> shopsByCity = new LinkedHashSet<StationeryShop>();
 	private static Set<String> shopsUrlSetByCity = new LinkedHashSet<String>();
-	private static Map<String , Set<String>> urlMapByCity = new TreeMap<String , Set<String>>();
-	private static Map<String , Set<String>> localitiesMapByCity = new TreeMap<String, Set<String>>();
-	
-	public enum City { Hyderabad };
+	private static Map<String, Set<String>> urlMapByCity = new TreeMap<String, Set<String>>();
+	private static Map<String, Set<String>> localitiesMapByCity = new TreeMap<String, Set<String>>();
 
+	public enum City {
+		Hyderabad
+	};
+
+	/*
+	 * Get all the docIds for a particular URL. Doc Ids are needed to hit the JD
+	 * Api to get the co-ordinates
+	 */
 	public static void getAllDocIDsInPage(String url) throws IOException {
 		Document idDoc = Jsoup.connect(url).get();
 
@@ -53,25 +61,35 @@ public class JDCrawler {
 		}
 
 	}
-	
+
+	// Get all the localities for a particular city
 	public static void getAllLocalities(String city) throws IOException {
-		Document locDoc = Jsoup.connect("http://www.propertykhazana.com/localities/hyderabad").get();
-		Elements localityElements = locDoc.select("a[href]");
-		
-		for(Element element : localityElements ) {
-			System.out.println(element.childNodes());
+		StringBuilder localityUrl = new StringBuilder("http://www.propertykhazana.com/localities/");
+		localityUrl.append(city.toLowerCase());
+		Document locDoc = Jsoup.connect(localityUrl.toString()).get();
+		Elements localityElements = locDoc.getElementsByAttributeValue("id", "localitylist");
+		Set<String> localitiesSet = new TreeSet<String>();
+		for (Element element : localityElements) {
+			for (Node data : element.childNodes()) {
+				String locality = data.toString().replaceAll("\\<.*?>", "").trim();
+				if (!locality.isEmpty() && !"".equals(locality))
+					localitiesSet.add(locality);
+			}
 		}
+		System.out.println(localitiesSet);
+		localitiesMapByCity.put(city, localitiesSet);
 	}
-	
-	public static void prepareUrlsForCities(String primaryUrl) {	
-		for(City city : City.values()) {
+
+	//
+	public static void prepareUrlsForCities(String primaryUrl) {
+		for (City city : City.values()) {
 			StringBuilder cityUrlBuilder = new StringBuilder();
 			cityUrlBuilder.append(primaryUrl).append(city).append("/Stationery-Shops/nct-1764/page-");
 			mainUrlMapByCity.put(city.toString(), cityUrlBuilder.toString());
 		}
 	}
-	
-	public static void preparePaginationUrlsCityWise(String baseUrl) {		
+
+	public static void preparePaginationUrlsCityWise(String baseUrl) {
 		for (int i = 1; i <= 50; i++) {
 			StringBuilder urlBuilder = new StringBuilder();
 			urlBuilder.append(baseUrl).append(i);
@@ -87,32 +105,24 @@ public class JDCrawler {
 			String countryCode = "IN";
 			String StateCode = "NA"; // valid only for USA. In our case "NA".
 
-			String final_url = base_url + "/" + doc_id + "/" + countryCode
-					+ "/" + city + "/" + StateCode;
+			String final_url = base_url + "/" + doc_id + "/" + countryCode + "/" + city + "/" + StateCode;
 			shopsUrlSetByCity.add(final_url);
 		}
 	}
 
-	public static void getAllShopsInCity(Set<String> shopsUrlSetByCity, String city)
-			throws JSONException, IOException {
+	public static void getAllShopsInCity(Set<String> shopsUrlSetByCity, String city) throws JSONException, IOException {
 		for (String url : shopsUrlSetByCity) {
 			JSONObject newJsonObject = readJsonFromUrl(url);
 			// System.out.println(newObject.getJSONObject("results").get("name"));
 			StationeryShop newShop = new StationeryShop();
-			newShop.setName(newJsonObject.getJSONObject("results").getString(
-					"name"));
-			newShop.setContact(newJsonObject.getJSONObject("results")
-					.getString("contact"));
-			newShop.setComplat(newJsonObject.getJSONObject("results")
-					.getString("complat"));
-			newShop.setComplong(newJsonObject.getJSONObject("results")
-					.getString("complong"));
-			newShop.setLandmark(newJsonObject.getJSONObject("results")
-					.getString("landmark"));
-			newShop.setAddress(newJsonObject.getJSONObject("results")
-					.getString("address").replaceAll(",", ";"));
+			newShop.setName(newJsonObject.getJSONObject("results").getString("name"));
+			newShop.setContact(newJsonObject.getJSONObject("results").getString("contact"));
+			newShop.setComplat(newJsonObject.getJSONObject("results").getString("complat"));
+			newShop.setComplong(newJsonObject.getJSONObject("results").getString("complong"));
+			newShop.setLandmark(newJsonObject.getJSONObject("results").getString("landmark"));
+			newShop.setAddress(newJsonObject.getJSONObject("results").getString("address").replaceAll(",", ";"));
 			newShop.setCity(city);
-			
+
 			System.out.println(newShop);
 
 			shopsByCity.add(newShop);
@@ -128,12 +138,10 @@ public class JDCrawler {
 		return sb.toString();
 	}
 
-	public static JSONObject readJsonFromUrl(String url) throws IOException,
-			JSONException {
+	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
 		InputStream is = new URL(url).openStream();
 		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
-					Charset.forName("UTF-8")));
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
 			String jsonText = readAll(rd);
 			JSONObject json = new JSONObject(jsonText);
 			return json;
@@ -141,32 +149,33 @@ public class JDCrawler {
 			is.close();
 		}
 	}
-	
+
 	public static void writeDataToCSV() throws IOException {
 		String csvFile = "..//JDCrawler//StationeryList_India.csv";
 		FileWriter writer = new FileWriter(csvFile);
-		
-		CSVUtils.writeLine(writer, Arrays.asList("Name", "Contact", "Lattitude","Longitude","Landmark","Address","City"));
-	     
-	     for(StationeryShop shop : shopsByCity) {
-	    	 List<String> list = new ArrayList<String>();
-	            list.add(shop.getName().replaceAll(",", ";"));
-	            list.add(shop.getContact().replaceAll(",", ";"));
-	            list.add(shop.getComplat());
-	            list.add(shop.getComplong());
-	            if(shop.getLandmark() != null) {
-	            	list.add(shop.getLandmark().replaceAll(",", ";"));
-	            }
-	            list.add(shop.getAddress().replaceAll(",", ";"));
-	            list.add(shop.getCity());
-	            
-	            CSVUtils.writeLine(writer, list);
-	     }
-	     
-	     writer.flush();
-	     writer.close();
+
+		CSVUtils.writeLine(writer,
+				Arrays.asList("Name", "Contact", "Lattitude", "Longitude", "Landmark", "Address", "City"));
+
+		for (StationeryShop shop : shopsByCity) {
+			List<String> list = new ArrayList<String>();
+			list.add(shop.getName().replaceAll(",", ";"));
+			list.add(shop.getContact().replaceAll(",", ";"));
+			list.add(shop.getComplat());
+			list.add(shop.getComplong());
+			if (shop.getLandmark() != null) {
+				list.add(shop.getLandmark().replaceAll(",", ";"));
+			}
+			list.add(shop.getAddress().replaceAll(",", ";"));
+			list.add(shop.getCity());
+
+			CSVUtils.writeLine(writer, list);
+		}
+
+		writer.flush();
+		writer.close();
 	}
-	
+
 	public static void cleanUp() {
 		docIdSet = new LinkedHashSet<String>();
 		paginationUrlSetByCity = new LinkedHashSet<String>();
@@ -174,28 +183,25 @@ public class JDCrawler {
 	}
 
 	public static void main(String[] args) throws IOException {
-//		prepareUrls(baseUrl);
-//		for(String url : urlSetByCity ){ 
-//			getAllDocIDsInPage(url);
-//		}
-//		prepareUrlsForCity("Pune", docIdSet);
-//		getAllShopsInCity(shopsUrlSetByCity);
-//		
-//		writeDataToCSV("Pune");
-		/*prepareUrlsForCities(primaryUrl);
-		Set<String> cities = mainUrlMapByCity.keySet();
-		for(String city : cities) {
-			String urlForCity = mainUrlMapByCity.get(city);
-			preparePaginationUrlsCityWise(urlForCity);
-			for(String url : paginationUrlSetByCity) {
-				getAllDocIDsInPage(url);
-			}
-			prepareJsonUrlsForCity(city, docIdSet);
-			getAllShopsInCity(shopsUrlSetByCity, city);
-			writeDataToCSV();
-			cleanUp();
-		}*/
-		
+		// prepareUrls(baseUrl);
+		// for(String url : urlSetByCity ){
+		// getAllDocIDsInPage(url);
+		// }
+		// prepareUrlsForCity("Pune", docIdSet);
+		// getAllShopsInCity(shopsUrlSetByCity);
+		//
+		// writeDataToCSV("Pune");
+		/*
+		 * prepareUrlsForCities(primaryUrl); Set<String> cities =
+		 * mainUrlMapByCity.keySet(); for(String city : cities) { String
+		 * urlForCity = mainUrlMapByCity.get(city);
+		 * preparePaginationUrlsCityWise(urlForCity); for(String url :
+		 * paginationUrlSetByCity) { getAllDocIDsInPage(url); }
+		 * prepareJsonUrlsForCity(city, docIdSet);
+		 * getAllShopsInCity(shopsUrlSetByCity, city); writeDataToCSV();
+		 * cleanUp(); }
+		 */
+
 		getAllLocalities("Hyderabad");
 	}
 
